0884d7106cd51ad9962f0ce64e877d97872794fc
aryanprince <A.Santy6483@student.leedsbeckett.ac.uk>
2022-03-14 14:36:29 +0000

    Basic funtionality

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-let current = document.getElementById("current");
-let needed = document.getElementById("needed");
-let cost = document.getElementById("cost");
+//requiredVP 67
 
-needed.oninput = () => {
-  let output = (parseFloat(needed.value) - parseFloat(current.value)) / 100;
-  cost.value = parseFloat(output.toFixed(2));
-};
+function process() {
+    var requiredVP = document.getElementById("requiredVP").value;
+    if (requiredVP >= 0 && requiredVP <= 999999) {
+        calculate(requiredVP);
+    } else {
+        alert("Please enter a number less than 1 million");
+        document.getElementById("vp475").value = "";
+        document.getElementById("vp1000").value = "";
+        document.getElementById("vp2050").value = "";
+        document.getElementById("vp2050").value = "";
+        document.getElementById("vp5350").value = "";
+        document.getElementById("vp11000").value = "";
+    }
+}
 
-fahrenheit.oninput = () => {
-  let output = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
-  celsius.value = parseFloat(output.toFixed(2));
-};
+function calculate(requiredVP) {
+    var vp11000 = parseInt(requiredVP / 11000);
+    requiredVP = requiredVP % 11000;
+    var vp5350 = parseInt(requiredVP / 5350);
+    requiredVP = requiredVP % 5350;
+    var vp3650 = parseInt(requiredVP / 3650);
+    requiredVP = requiredVP % 3650;
+    var vp2050 = parseInt(requiredVP / 2050);
+    requiredVP = requiredVP % 2050;
+    var vp1000 = parseInt(requiredVP / 1000);
+    requiredVP = requiredVP % 1000;
+    var vp475 = parseInt(requiredVP / 475);
 
-document.getElementById("calculate").onclick = function() {myFunction()};
-
-function myFunction() {
-  document.getElementById("calculate").innerHTML = "YOU CLICKED ME!";
+    document.getElementById("vp475").value = vp475;
+    document.getElementById("vp1000").value = vp1000;
+    document.getElementById("vp2050").value = vp2050;
+    document.getElementById("vp3650").value = vp3650;
+    document.getElementById("vp5350").value = vp5350;
+    document.getElementById("vp11000").value = vp11000;
 }
