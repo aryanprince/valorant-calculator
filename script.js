ce20a5d208eb9711d17bd99db98d4d504b4480df
aryanprince <aryanprincesanty@icloud.com>
2022-03-19 00:43:45 +0000

    Working modal dialogue

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ function process() {
 }
 
 function calculate(requiredVP) {
+    document.getElementById("exampleModalLabel").innerHTML = "Cost for " + requiredVP + " VP";
+
     var vp11000 = parseInt(requiredVP / 11000);
     requiredVP = requiredVP % 11000;
     var vp5350 = parseInt(requiredVP / 5350);
@@ -30,16 +32,23 @@ function calculate(requiredVP) {
     var noofvp5350 = vp5350;
     var noofvp11000 = vp11000;
 
-    if (document.getElementById("currency").options[document.getElementById("currency").selectedIndex].value == "usd") {
-        document.getElementById("cost").value = "$ " + (vp475 * 5 + vp1000 * 10 + vp2050 * 20 + vp3650 * 35 + vp5350 * 50 + vp11000 * 100);
+    var total_USD = vp475 * 5 + vp1000 * 10 + vp2050 * 20 + vp3650 * 35 + vp5350 * 50 + vp11000 * 100;
+    var total_GBP = vp475 * 4.5 + vp1000 * 9 + vp2050 * 18 + vp3650 * 30 + vp5350 * 45 + vp11000 * 90;
+    var total_INR = vp475 * 399 + vp1000 * 799 + vp2050 * 1599 + vp3650 * 2699 + vp5350 * 3999 + vp11000 * 7900;
+    var total_noof = noofvp475 + noofvp1000 + noofvp2050 + noofvp3650 + noofvp5350 + noofvp11000;
+
+    if (document.getElementById("currency").options[document.getElementById("currency").selectedIndex].value == "USD") {
+        document.getElementById("message").innerHTML = "$ " + total_USD;
     }
-    if (document.getElementById("currency").options[document.getElementById("currency").selectedIndex].value == "gbp") {
-        document.getElementById("cost").value = "£ " + (vp475 * 4.5 + vp1000 * 9 + vp2050 * 18 + vp3650 * 30 + vp5350 * 45 + vp11000 * 90);
+    if (document.getElementById("currency").options[document.getElementById("currency").selectedIndex].value == "GBP") {
+        document.getElementById("message").innerHTML = "£ " + total_GBP;
     }
-    if (document.getElementById("currency").options[document.getElementById("currency").selectedIndex].value == "inr") {
-        document.getElementById("cost").value = "₹ " + (vp475 * 399 + vp1000 * 799 + vp2050 * 1599 + vp3650 * 2699 + vp5350 * 3999 + vp11000 * 7900);
+    if (document.getElementById("currency").options[document.getElementById("currency").selectedIndex].value == "INR") {
+        document.getElementById("message").innerHTML = "₹ " + total_INR;
     }
-    if (document.getElementById("currency").options[document.getElementById("currency").selectedIndex].value == "qar") {
-        document.getElementById("cost").value = "QR " + ((vp475 * 5 + vp1000 * 10 + vp2050 * 20 + vp3650 * 35 + vp5350 * 50 + vp11000 * 100) * 3.64).toFixed(2);
+    if (document.getElementById("currency").options[document.getElementById("currency").selectedIndex].value == "QAR") {
+        document.getElementById("message").innerHTML = "QR " + total_USD * 3.64;
     }
+
+    document.getElementById("amount").innerHTML = noofvp475 + "x 475" + ", " + noofvp1000 + "x 1000" + ", " + noofvp2050 + "x 2050" + ", " + noofvp3650 + "x 3650" + ", " + noofvp5350 + "x 5350" + ", " + noofvp11000 + "x 11000" + ", ";
 }
